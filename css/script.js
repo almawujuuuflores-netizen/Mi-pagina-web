08e2bcbe80c2756280d77364748043c0096fad7b
almawujuuuflores-netizen <almawujuuuflores@gmail.com>
2025-11-04 17:53:48 -0600

    Add files via upload

---
+++ css/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("bienvenidaBtn");
+    if (btn) {
+        btn.addEventListener("click", () => {
+            alert("💗 Gracias por ser parte de nuestra historia. ¡Disfruta este rinconcito hecho con amor, café y amistad! ☕✨");
+
+        });
+    }
+});
